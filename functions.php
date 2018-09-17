3bb67086ca55c224b064ad5b8ea25a28578733cc
Tristan Jarrett <tristan.jarrett@outlook.com>
2018-09-17 11:19:13 +0100

    Remove comments

--- functions.php
+++ functions.php
@@ -31,15 +31,30 @@
 	add_theme_support('post-thumbnails');
 
 	/**
-	 * remove non-needed menu items
+	 * remove comments everywhere
 	 */
-	function menu_remove () {
-		remove_menu_page('edit-comments.php');
+	// removes from admin menu
+	function my_remove_admin_menus() {
+		remove_menu_page( 'edit-comments.php' );
 	}
-	add_action('admin_menu', 'menu_remove');
+	add_action( 'admin_menu', 'my_remove_admin_menus' );
+
+	// removes from post and pages
+	function remove_comment_support() {
+		remove_post_type_support( 'post', 'comments' );
+		remove_post_type_support( 'page', 'comments' );
+	}
+	add_action('init', 'remove_comment_support', 100);
+
+	// removes from admin bar
+	function mytheme_admin_bar_render() {
+		global $wp_admin_bar;
+		$wp_admin_bar->remove_menu('comments');
+	}
+	add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
 
 	/**
-	 * Ccustom post type for branches
+	 * custom post type for branches
 	 */
 	function pbs_post_branch() {
 	  register_post_type( 'branch',
