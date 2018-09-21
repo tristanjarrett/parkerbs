8eae40096e24732654a0cf845df910fe30665f77
Tristan Jarrett <tristan.jarrett@outlook.com>
2018-09-21 16:42:09 +0100

    Disabled screen options for non admins

--- inc/functions/core/post-options.php
+++ inc/functions/core/post-options.php
@@ -3,3 +3,14 @@
  * enable post thumbnails
  */
 add_theme_support('post-thumbnails');
+
+/**
+ * Remove Screen Options (Non-Admins)
+ */
+function wpb_remove_screen_options() {
+  if(!current_user_can('manage_options')) {
+    return false;
+  }
+  return true;
+}
+add_filter('screen_options_show_screen', 'wpb_remove_screen_options');
