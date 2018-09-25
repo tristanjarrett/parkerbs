15658e1671b6344c53ebaeb36626bbdaec43c32c
Tristan Jarrett <tristan.jarrett@outlook.com>
2018-09-25 12:38:35 +0100

    Update

--- 404.php
+++ 404.php
@@ -1,20 +1,29 @@
 <?php get_header(); ?>
 
-	<div class="container-fluid">
+	<div class="pbs-404-section">
 
-		<div class="row pbs-404">
+		<div class="container-fluid">
+
 			<div class="col pbs-404-text">
+
 				<h1>Oops!</h1>
+
 				<h3>Sorry we couldn't seem to find the page you requested.</h3>
+				
 				<h5>Error code: 404 (page not found)</h5>
+
 				<hr>
+
 				<h5>Here are some helpful links to get you back on track</h5>
-				<a href="<?php echo get_bloginfo('wpurl'); ?>">Return Home</a>
-				<br>
-				<a href="<?php echo get_bloginfo('wpurl') . '/contact-us'; ?>">Contact Us</a>
-				<br>
-				<a href="<?php echo get_bloginfo('wpurl') . '/branches'; ?>">Store Finder</a>
+
+				<ul>
+					<li><a href="<?php echo get_bloginfo('wpurl'); ?>">Return Home</a></li>
+					<li><a href="<?php echo get_bloginfo('wpurl') . '/contact-us'; ?>">Contact Us</a></li>
+					<li><a href="<?php echo get_bloginfo('wpurl') . '/branches'; ?>">Store Finder</a></li>
+				</ul>
+
 			</div>
+
 		</div>
 
 	</div>
