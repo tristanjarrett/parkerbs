078d2213878d0b542bc2126f65be47b2f8dcea30
Tristan Jarrett <tristan.jarrett@outlook.com>
2018-10-17 15:26:48 +0100

    Update page-contact-us.php

--- page-contact-us.php
+++ page-contact-us.php
@@ -28,8 +28,8 @@
 
             <label>Contact:</label>
             <ul>
-              <li>0844 257 7000</li>
-              <li>info@parkerbs.com</li>
+              <li><a href="tel:08442577000">0844 257 7000</a></li>
+              <li><a href="mailto:info@parkerbs.com">info@parkerbs.com</a></li>
             </ul>
 
             <label>Office Hours:</label>
@@ -41,9 +41,11 @@
         </div>
 
         <div class="col-md-6 col-lg-8">
-          map
+
+
+
         </div>
-        
+
       </div>
 
 
