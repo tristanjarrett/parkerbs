fbbc0474f7953c71618054cc0169511ea090e2bf
Tristan Jarrett <tristan.jarrett@outlook.com>
2018-10-31 16:24:50 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
 				<div class="col-sm-6 col-lg-3">
 					<h5>My Account</h5>
 					<ul>
-						<li><a href="#">Account Application</a></li>
-						<li><a href="#">Request Login</a></li>
-						<li><a href="#">View My Account</a></li>
-						<li><a href="#">Password Recovery</a></li>
+						<li><a href="<?php echo site_url() . '/account-application'; ?>">Account Application</a></li>
+						<li><a href="<?php echo site_url() . '/request-login'; ?>">Request Login</a></li>
+						<li><a href="https://account.parkerbs.com">View My Account</a></li>
+						<li><a href="https://account.parkerbs.com">Password Recovery</a></li>
 					</ul>
 					<a href="<?php echo site_url(); ?>">
 						<img src="<?php echo get_bloginfo( 'template_directory' ) . '/images/core/logo.png'; ?>" alt="Logo" height="100">
@@ -74,7 +74,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<!-- ./jQuery, Popper.js & Bootstrap -->
-	
+
 	<?php wp_footer(); ?>
 
 	</body>
