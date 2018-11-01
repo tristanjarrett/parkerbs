479405925ac476ee1e0b54aec103e60a25235d9f
Tristan Jarrett <tristan.jarrett@outlook.com>
2018-11-01 10:04:42 +0000

    Updates to single page

--- page.php
+++ page.php
@@ -1,15 +1,19 @@
 <?php get_header(); ?>
 
-	<div class="container-fluid mt-3">
-		<h3><?php the_title(); ?></h3>
-		<hr>
-		<?php
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				the_content();
-			endwhile;
-		endif;
-		?>
+	<div class="pbs-general-page">
+
+		<div class="container-fluid">
+			<h3><?php the_title(); ?></h3>
+			<hr>
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+					the_content();
+				endwhile;
+			endif;
+			?>
+		</div>
+
 	</div>
 
 <?php get_footer(); ?>
