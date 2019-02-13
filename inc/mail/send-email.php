deee063504884912d43d563a1e4e41f90b54bfcc
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-02-13 14:30:28 +0000

    Added lines to remove file from server

--- inc/mail/send-email.php
+++ inc/mail/send-email.php
@@ -1,30 +1,32 @@
 <?php
 
-  //response generation function
+  // response generation function
   $response = "";
 
-  //function to generate response
+  // function to generate response
   function generate_response($type, $message){
 
     global $response;
 
     if($type == "success") {
       $response = "<div class='alert alert-success' role='alert'>{$message}</div>";
+      unlink(WP_CONTENT_DIR . '/uploads/' . basename($_FILES['message_resume']['name'])); // delete file from server
     }
     else {
       $response = "<div class='alert alert-danger' role='alert'>{$message}</div>";
+      unlink(WP_CONTENT_DIR . '/uploads/' . basename($_FILES['message_resume']['name'])); // delete file from server
     }
 
   }
 
-  //response messages
+  // response messages
   $not_human       = "Human verification incorrect. Please try again.";
   $missing_content = "Please complete all required fields.";
   $email_invalid   = "Email address invalid.";
   $message_unsent  = "An error occurred, please try again. If this continues then please report it to <a href='mailto:info@parkerbs.com'>info@parkerbs.com</a>";
   $message_sent    = "Thank you, your application has been submitted.";
 
-  //user posted variables
+  // user posted variables
   $fname   = $_POST['message_fname'];
   $lname   = $_POST['message_lname'];
   $email   = $_POST['message_email'];
@@ -35,8 +37,7 @@
   // necessary for sending attachment
   move_uploaded_file($_FILES["message_resume"]["tmp_name"], WP_CONTENT_DIR . '/uploads/' . basename($_FILES['message_resume']['name']));
 
-  //php mailer variables
-  //$to = get_option('admin_email');
+  // php mailer variables
   $to          = "webcareers@parkerbs.com";
   $subject     = "Application for " . get_the_title() . " at " . get_post_meta($post->ID, 'custom_input', true) . " via the website";
   $headers     = 'From: '. $email . "\r\n" . 'Reply-To: ' . $email . "\r\n";
@@ -45,15 +46,15 @@
 
   if(!$human == 0) {
     if($human != 2) {
-      generate_response("error", $not_human); //not human!
+      generate_response("error", $not_human); // not human!
     }
     else {
       $sent = wp_mail($to, $subject, $body, $headers, $attachments);
       if($sent) {
-        generate_response("success", $message_sent); //message sent!
+        generate_response("success", $message_sent); // message sent!
       }
       else {
-        generate_response("error", $message_unsent); //message wasn't sent
+        generate_response("error", $message_unsent); // message wasn't sent
       }
     }
   }
