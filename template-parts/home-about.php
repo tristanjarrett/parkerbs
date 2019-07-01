ad0f403fbb3956947359408a78c2a589b9cdfcc6
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-07-01 12:47:21 +0100

    Update home-about.php

--- template-parts/home-about.php
+++ template-parts/home-about.php
@@ -1,5 +1,9 @@
 <div class="pbs-home-about">
   <div class="container-fluid">
-    <p class="pbs-focus-p pbs-cut-p">Parker Building Supplies is building Sussex and Kent’s future, one brick and block at a time. We supply a huge range of building, plumbing and joinery materials alongside specialist ironmongery, kitchens and bathrooms from 15 locations across the region – so wherever you are working you’ll find a Parker's branch with knowledgeable staff ready to help and a delivery service second to none. We are proud to be Sussex and Kent’s favourite independent builder’s merchant.</p>
+    <p class="pbs-focus-p">Parker Building Supplies is building Sussex and Kent’s future, one brick and block at a time. We supply a huge range of building, plumbing and joinery materials alongside specialist ironmongery, kitchens and bathrooms across the region – so wherever they’re working, our customers will find a Parkers branch with knowledgeable staff ready to help and a delivery service second to none.</p>
+    <p class="pbs-focus-p">We are proud to be Sussex and Kent’s favourite independent builder’s merchant and, following the acquisiton of Stamco in June 2019, we are also now one of the largest independent builder’s merchants in the South East with more than 450 colleagues working across 20+ locations.</p>
+    <p class="pbs-focus-p">We are also proud supporters of local communities and sports and active fundraisers for the Teenage Cancer Trust.</p>
+    <p class="pbs-focus-p">Please use our branch finder to find your nearest branch. You’ll find details about the branch, as well as specialisms, our branch managers and contact details - so drop in or call to access our expert colleagues.</p>
+    <p class="pbs-focus-p pbs-cut-p">Our product pages provide details of our extensive range of more than 10,000 products from bricks and blocks to bathrooms, ironmongery joinery and plumbing.</p>
   </div>
 </div>
