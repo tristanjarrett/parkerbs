0072966b808d8038ebf76061257801f4a74ae1f7
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-08-14 14:21:24 +0100

    Case Studies

--- single-case-study.php
+++ single-case-study.php
@@ -15,16 +15,19 @@
 	<div class="pbs-case-study-page">
 
 		<div class="container-fluid">
-			<h3><?php the_title(); ?></h3>
-			<hr>
 
-			<?php
-			if ( have_posts() ) :
-				while ( have_posts() ) : the_post();
-					the_content();
-				endwhile;
-			endif;
-			?>
+			<div class="pbs-content">
+				<h3><?php the_title(); ?></h3>
+				<hr>
+
+				<?php
+				if ( have_posts() ) :
+					while ( have_posts() ) : the_post();
+						the_content();
+					endwhile;
+				endif;
+				?>
+			</div>
 
 		</div>
 
