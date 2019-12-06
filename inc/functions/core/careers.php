5fd2e18d3ab2790fc8849fcef1d3f61ab2bff890
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-12-06 21:49:24 +0000

    Update careers.php

--- inc/functions/core/careers.php
+++ inc/functions/core/careers.php
@@ -12,17 +12,14 @@ function pbs_linkedin_meta() {
 }
 
 // Create special page templates by post type and category name
-add_filter('single_template', 'check_for_category_single_template');
-function check_for_category_single_template( $t )
-{
-  foreach( (array) get_the_category() as $cat ) 
-  { 
+function check_for_category_single_template( $t ) {
+  foreach( (array) get_the_category() as $cat ) { 
     if ( file_exists(get_stylesheet_directory() . "/single-vacancy-{$cat->slug}.php") ) return get_stylesheet_directory() . "/single-vacancy-{$cat->slug}.php"; 
-    if($cat->parent)
-    {
+    if($cat->parent) {
       $cat = get_the_category_by_ID( $cat->parent );
       if ( file_exists(get_stylesheet_directory() . "/single-vacancy-{$cat->slug}.php") ) return get_stylesheet_directory() . "/single-vacancy-{$cat->slug}.php";
     }
   } 
   return $t;
 }
+add_filter('single_template', 'check_for_category_single_template');
