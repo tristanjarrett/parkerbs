f47f6a5109b917d9aae567d0b541330dc7a7d9f9
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-12-06 22:03:18 +0000

    Brand updates

--- inc/functions/core/page-title.php
+++ inc/functions/core/page-title.php
@@ -14,3 +14,27 @@ function pbs_title() {
    the_title(); echo ' | '; echo $site_name;
  endif;
 }
+
+function stamco_title() {
+  if (is_page()) {
+    echo 'Vacancies | Stamco Building Supplies';
+  } else if (is_single()) {
+    the_title(); echo ' | Stamco Building Supplies';
+  }
+}
+
+function fairalls_title() {
+  if (is_page()) {
+    echo "Vacancies | Fairalls Builders' Merchants";
+  } else if (is_single()) {
+    the_title(); echo " | Fairalls Builders' Merchants";
+  }
+}
+
+function chandlers_title() {
+  if (is_page()) {
+    echo 'Vacancies | Chandlers Building Supplies';
+  } else if (is_single()) {
+    the_title(); echo ' | Chandlers Building Supplies';
+  }
+}
